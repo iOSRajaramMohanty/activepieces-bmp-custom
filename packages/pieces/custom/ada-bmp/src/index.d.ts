bd6bad8ee57abaa2558c69c63fc9223a017884ff
Rajaram Mohanty <rajarammohanty99@gmail.com>
2026-03-26 17:02:59 +0530

    fix(bmp): deleted the build files

--- packages/pieces/custom/ada-bmp/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const adaBmpAuth: import("@activepieces/pieces-framework").CustomAuthProperty<{
-    apiToken: import("@activepieces/pieces-framework").ShortTextProperty<true>;
-    environment: import("@activepieces/pieces-framework").StaticDropdownProperty<string, true>;
-}>;
-export declare const adaBmp: import("@activepieces/pieces-framework").Piece<import("@activepieces/pieces-framework").CustomAuthProperty<{
-    apiToken: import("@activepieces/pieces-framework").ShortTextProperty<true>;
-    environment: import("@activepieces/pieces-framework").StaticDropdownProperty<string, true>;
-}>>;
