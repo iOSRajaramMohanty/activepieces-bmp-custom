31b80eb550020c8a4bf6cf2a44fb3d9980437700
Rajaram Mohanty <rajarammohanty99@gmail.com>
2026-03-27 14:39:21 +0530

    fix(bmp-sdk): ui related issues

--- packages/web/src/components/ui/sheet.tsx
+++ packages/web/src/components/ui/sheet.tsx
@@ -34,6 +34,7 @@ function SheetOverlay({
 }: React.ComponentProps<typeof SheetPrimitive.Overlay>) {
   return (
     <SheetPrimitive.Overlay
+      data-radix-portal=""
       data-slot="sheet-overlay"
       className={cn(
         'fixed inset-0 z-50 bg-black/50 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:animate-in data-[state=open]:fade-in-0',
@@ -58,6 +59,7 @@ function SheetContent({
     <SheetPortal>
       <SheetOverlay />
       <SheetPrimitive.Content
+        data-radix-portal=""
         data-slot="sheet-content"
         className={cn(
           'fixed z-50 flex flex-col gap-4 bg-background shadow-lg transition ease-in-out data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:animate-in data-[state=open]:duration-500',
